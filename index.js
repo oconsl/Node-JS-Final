b115b61f504af093556f86f3f7cea4e9dfad4a6f
Santiago Ocón <ocon.santiago@gmail.com>
2022-08-07 21:31:54 -0300

    Deploy settings Railway.app test.

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Book = require('./models/bookModel');
 const User = require('./models/userModel');
-const bodyParser = require('body-parser');
 const bookRouter = require('./routes/bookRouter')(Book);
 const userRouter = require('./routes/userRouter')(User);
 const cors = require('cors');
-const expressJwt = require('express-jwt');
+const { expressjwt: jwt } = require("express-jwt");
 require('dotenv').config();
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/bookAPI');
+mongoose.connect(process.env.DB_PROD);
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.all(
   '/api/*',
-  expressJwt({
+  jwt({
     secret: process.env.SIGNATURE,
     algorithms: ['HS256'],
   }).unless({
